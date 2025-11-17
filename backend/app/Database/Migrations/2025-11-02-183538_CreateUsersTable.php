f116b593d433c54c6d11cdfb40df785967dcd908
Lawrence Blasco <79191129+BulletSplash@users.noreply.github.com>
2025-11-17 19:38:33 +0800

    feat(account table seeder): Added a seeder for accounts table
    - refactor migration

--- backend/app/Database/Migrations/2025-11-02-183538_CreateUsersTable.php
+++ backend/app/Database/Migrations/2025-11-02-183538_CreateUsersTable.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
                 'unsigned'       => true,   // optional
                 'auto_increment' => true,   // optional if you want auto counting, but important for the id
                 'null'           => false,  // not needed for id, but needed for most
-                'default'        => 1,      // optional
             ],
 
             'fullname' => [
@@ -53,9 +52,6 @@ class CreateUsersTable extends Migration
         ]);
 
         $this->forge->addKey('id', true);
-        $this->forge->addUniqueKey('id');
-        $this->forge->addUniqueKey('email');
-        $this->forge->addUniqueKey('password');
         $this->forge->createTable('accounts', true);
     }
 
