e85643f78d5699ee092ca5409af9d064e5b147e8
Lawrence Blasco <79191129+BulletSplash@users.noreply.github.com>
2025-11-24 17:05:13 +0800

    refactor(database) : add a new data to database
    - refactor userseeder

--- backend/app/Database/Seeds/UsersSeeder.php
+++ backend/app/Database/Seeds/UsersSeeder.php
@@ -11,40 +11,46 @@ class UsersSeeder extends Seeder
         $now = date('Y-m-d H:i:s');
         // if you want password that is hashed
         $password = password_hash('Password123!', PASSWORD_DEFAULT);
+        $admin_password = password_hash('admin', PASSWORD_DEFAULT);
 
         $dataYouWannaInsert = [
                 [
-                    'fullname' => 'Juan Dela Cruz',
-                    'email' => 'data@gmail.com',
-                    'password' => $password,
+                    'fullname' => 'Admin User',
+                    'email' => 'admin@gmail.com',
+                    'password' => $admin_password,
+                    'type' => 'manager',
                     'created_at' => $now,
                     'updated_at' => $now,
                 ],
                 [
-                    'fullname' => 'Juan Dela Cruz',
+                    'fullname' => 'Juan DelaCruz',
                     'email' => 'data@gmail.com',
                     'password' => $password,
+                    'type' => 'client',
                     'created_at' => $now,
                     'updated_at' => $now,
                 ],
                 [
-                    'fullname' => 'Juan Dela Cruz',
+                    'fullname' => 'Juan DelaCruz',
                     'email' => 'data@gmail.com',
                     'password' => $password,
+                    'type' => 'client',
                     'created_at' => $now,
                     'updated_at' => $now,
                 ],
                 [
-                    'fullname' => 'Juan Dela Cruz',
+                    'fullname' => 'Juan DelCruz',
                     'email' => 'data@gmail.com',
                     'password' => $password,
+                    'type' => 'client',
                     'created_at' => $now,
                     'updated_at' => $now,
                 ],
                 [
-                    'fullname' => 'Juan Dela Cruz',
+                    'fullname' => 'Juan DelaCruz',
                     'email' => 'data@gmail.com',
                     'password' => $password,
+                    'type' => 'client',
                     'created_at' => $now,
                     'updated_at' => $now,
                 ]
